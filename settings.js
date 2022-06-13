bacf3d57fb2a07b846adadccb8681278fcaba112
BOT-99V <106417115+BOT-99V@users.noreply.github.com>
2022-06-13 07:23:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,10 +19,10 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['919072501166']
+global.premium = ['919072501166']
+global.ownernomer = '919072501166'
+global.ownername = '🐦ADHI'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
