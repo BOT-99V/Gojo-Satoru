01a0efe82ae775c89c0b1d2946202b1cf98a6b85
BOT-99V <106417115+BOT-99V@users.noreply.github.com>
2022-06-13 08:13:16 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,13 +19,14 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.owner = ['919072501166']
+global.premium = ['918078227355]
+global.ownernomer = '919072501166'
+global.ownername = 'ADHISER'
+global.botname = 'VILLAN'
+global.footer = 'VILLAN'
+global.ig = 'https://github.com/BOT-99V
+		  '
 global.region = 'India, South India, Kerala'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
 global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
